b4ab4e361dbb578857cbf026bb8eae70ff129430
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-04-09 21:24:35 -0300

    create menu action bar

--- app/src/main/java/com/example/rodri/todo/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/rodri/todo/ui/activity/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.rodri.todo.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.RelativeLayout;
 
 import com.example.rodri.todo.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        RelativeLayout menuLayout = (RelativeLayout) menu.findItem(R.id.whitePlus).getActionView();
+
+        return true;
     }
 }
