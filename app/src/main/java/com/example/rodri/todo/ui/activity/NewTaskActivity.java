44924f398c8e5ab8e7c53ddf235aec429aea53db
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-04-11 13:45:54 -0300

    add SeekBar for number priority to new_task.xml

--- app/src/main/java/com/example/rodri/todo/ui/activity/NewTaskActivity.java
+++ app/src/main/java/com/example/rodri/todo/ui/activity/NewTaskActivity.java
@@ -2,6 +2,8 @@ package com.example.rodri.todo.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.rodri.todo.R;
 
@@ -15,7 +17,28 @@ public class NewTaskActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_task);
 
-        
+        SeekBar prioritySeekBar = (SeekBar) findViewById(R.id.prioritySeekBar);
+        final TextView numPriority = (TextView) findViewById(R.id.txtNumPriority);
+        numPriority.setText(String.valueOf(prioritySeekBar.getProgress()));
+
+        prioritySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+                progress = progressValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                numPriority.setText(String.valueOf(progress));
+            }
+        });
 
     }
 
