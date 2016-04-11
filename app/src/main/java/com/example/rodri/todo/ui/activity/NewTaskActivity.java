d5c6bcdf9c63ed789a7abc6385db42f3d4cddb8b
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-04-11 14:50:07 -0300

    add DatePickerDialog to new_activity.xml. Create class Util

--- app/src/main/java/com/example/rodri/todo/ui/activity/NewTaskActivity.java
+++ app/src/main/java/com/example/rodri/todo/ui/activity/NewTaskActivity.java
@@ -1,24 +1,42 @@
 package com.example.rodri.todo.ui.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rodri.todo.R;
+import com.example.rodri.todo.util.Util;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * Created by rodri on 4/11/2016.
  */
 public class NewTaskActivity extends Activity {
 
+    private SeekBar prioritySeekBar;
+    private TextView numPriority;
+    private Button setDueDate;
+    private Button confirm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Util.setFullScreen(NewTaskActivity.this);
         setContentView(R.layout.new_task);
+        initialize();
 
-        SeekBar prioritySeekBar = (SeekBar) findViewById(R.id.prioritySeekBar);
-        final TextView numPriority = (TextView) findViewById(R.id.txtNumPriority);
+
         numPriority.setText(String.valueOf(prioritySeekBar.getProgress()));
 
         prioritySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -40,6 +58,33 @@ public class NewTaskActivity extends Activity {
             }
         });
 
+        setDueDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        String date = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                        Toast.makeText(NewTaskActivity.this, "The chosen date is: " + date, Toast.LENGTH_LONG).show();
+                    }
+                };
+
+                Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+                DatePickerDialog datePicker = new DatePickerDialog(NewTaskActivity.this, R.style.AppTheme, datePickerListener,
+                        cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+
+                datePicker.show();
+
+            }
+        });
+
+    }
+
+    private void initialize() {
+        prioritySeekBar = (SeekBar) findViewById(R.id.prioritySeekBar);
+        numPriority = (TextView) findViewById(R.id.txtNumPriority);
+        setDueDate = (Button) findViewById(R.id.btSetDueDate);
+        confirm = (Button) findViewById(R.id.btConfirm);
     }
 
 }
